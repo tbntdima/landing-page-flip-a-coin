018d110f85b0379afaa5991962bbedd350510993
Dmitriy An <an.dmitriy01@gmail.com>
2019-05-25 21:54:11 +0200

    Updates

---
+++ precache-manifest.b5cbaedbda1dea9671c88fda1e91da63.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "e785296381aa8977c9da",
+    "url": "/landing/flip-a-coin-w1/static/css/main.dbbf99a5.chunk.css"
+  },
+  {
+    "revision": "e785296381aa8977c9da",
+    "url": "/landing/flip-a-coin-w1/static/js/main.e7852963.chunk.js"
+  },
+  {
+    "revision": "9998823ef3976e1091de",
+    "url": "/landing/flip-a-coin-w1/static/css/1.77b23a4b.chunk.css"
+  },
+  {
+    "revision": "9998823ef3976e1091de",
+    "url": "/landing/flip-a-coin-w1/static/js/1.9998823e.chunk.js"
+  },
+  {
+    "revision": "5e79dd2e2cb7253f437d",
+    "url": "/landing/flip-a-coin-w1/static/js/runtime~main.5e79dd2e.js"
+  },
+  {
+    "revision": "383998df3a9519e5dc91044501f33a46",
+    "url": "/landing/flip-a-coin-w1/static/media/coin-default.383998df.png"
+  },
+  {
+    "revision": "faa308450d605dec5df041ef9b5b0a2a",
+    "url": "/landing/flip-a-coin-w1/static/media/coin-plentiful-treasure.faa30845.png"
+  },
+  {
+    "revision": "494eddd54845a7682e3b334c596fab06",
+    "url": "/landing/flip-a-coin-w1/static/media/coin-t-rex.494eddd5.png"
+  },
+  {
+    "revision": "a179b65bd996f904886991a222df9a32",
+    "url": "/landing/flip-a-coin-w1/static/media/coin-empress-wu.a179b65b.png"
+  },
+  {
+    "revision": "3b519b8ca630b35263bb2535f24739bb",
+    "url": "/landing/flip-a-coin-w1/static/media/coin-coyote-cash.3b519b8c.png"
+  },
+  {
+    "revision": "1e9ebfe1c24a94c2b915313c83197708",
+    "url": "/landing/flip-a-coin-w1/static/media/plentiful-treasure.1e9ebfe1.jpg"
+  },
+  {
+    "revision": "950387ed1f6984d882031a214dab1b67",
+    "url": "/landing/flip-a-coin-w1/static/media/t-rex.950387ed.jpg"
+  },
+  {
+    "revision": "55e8dedcfdc7c7f3d1ac7b5b31421dd0",
+    "url": "/landing/flip-a-coin-w1/static/media/empress-wu.55e8dedc.jpg"
+  },
+  {
+    "revision": "e1d00e9013268fb52b33455d3ab31f84",
+    "url": "/landing/flip-a-coin-w1/static/media/coyote-cash.e1d00e90.jpg"
+  },
+  {
+    "revision": "a4cbb33502e4c157cf1d4697ceb67adc",
+    "url": "/landing/flip-a-coin-w1/static/media/flip-again-button.a4cbb335.png"
+  },
+  {
+    "revision": "c4f6d4afc165b5a7e96d5fb47edac8a1",
+    "url": "/landing/flip-a-coin-w1/static/media/ptflip290.c4f6d4af.png"
+  },
+  {
+    "revision": "00a5530529017c52213203244638cd63",
+    "url": "/landing/flip-a-coin-w1/static/media/trexflip280.00a55305.png"
+  },
+  {
+    "revision": "8cce28f16d53ef374b655d71c542cf06",
+    "url": "/landing/flip-a-coin-w1/static/media/ewflip275.8cce28f1.png"
+  },
+  {
+    "revision": "e196ec3d30c02be8c23873f2987dab87",
+    "url": "/landing/flip-a-coin-w1/static/media/coyoteflip275.e196ec3d.png"
+  },
+  {
+    "revision": "7ec88412ab00838c760ceab283378989",
+    "url": "/landing/flip-a-coin-w1/static/media/ptflip320.7ec88412.png"
+  },
+  {
+    "revision": "d59ae43b2f6766c7ff69110a4d6f2cf9",
+    "url": "/landing/flip-a-coin-w1/static/media/trexflip310.d59ae43b.png"
+  },
+  {
+    "revision": "b440d91a9dfd09a34b4a6e40df16c9c0",
+    "url": "/landing/flip-a-coin-w1/static/media/ewflip330.b440d91a.png"
+  },
+  {
+    "revision": "3c94979ae665a83bacc59a033de7a456",
+    "url": "/landing/flip-a-coin-w1/static/media/coyoteflip315.3c94979a.png"
+  },
+  {
+    "revision": "b2bf8313484feb30bf06b578bd626a0c",
+    "url": "/landing/flip-a-coin-w1/static/media/logo.b2bf8313.png"
+  },
+  {
+    "revision": "f7518ff3cc5473c2014c0574332e762b",
+    "url": "/landing/flip-a-coin-w1/static/media/game-title.f7518ff3.png"
+  },
+  {
+    "revision": "7004b243f7aad28e7fce3c8d44c229d1",
+    "url": "/landing/flip-a-coin-w1/static/media/flip-button.7004b243.png"
+  },
+  {
+    "revision": "158253c3f3dda55e9deda64e4f5a6c2b",
+    "url": "/landing/flip-a-coin-w1/static/media/curtain-left.158253c3.png"
+  },
+  {
+    "revision": "132f77306b6fb0a5b620bff2dc95c78a",
+    "url": "/landing/flip-a-coin-w1/static/media/curtain-right.132f7730.png"
+  },
+  {
+    "revision": "017e9c0f3a95776921fe842768ebd7d9",
+    "url": "/landing/flip-a-coin-w1/static/media/coin-glow.017e9c0f.png"
+  },
+  {
+    "revision": "e6f82fd380f02e22514c588407168a60",
+    "url": "/landing/flip-a-coin-w1/static/media/coin-stars.e6f82fd3.png"
+  },
+  {
+    "revision": "c4f6d4afc165b5a7e96d5fb47edac8a1",
+    "url": "/landing/flip-a-coin-w1/static/media/default.c4f6d4af.png"
+  },
+  {
+    "revision": "ea49840d46217df6ad1c9edca5f47038",
+    "url": "/landing/flip-a-coin-w1/static/media/promo-table-left.ea49840d.svg"
+  },
+  {
+    "revision": "f6358d5a7a5421e4b8b1901af59b997d",
+    "url": "/landing/flip-a-coin-w1/static/media/promo-table-right.f6358d5a.svg"
+  },
+  {
+    "revision": "44d7b2312bbd3ff2fe44768cf7b9b041",
+    "url": "/landing/flip-a-coin-w1/static/media/promo-header.44d7b231.svg"
+  },
+  {
+    "revision": "24cc86911e815619ac6b3786ef2fd9d4",
+    "url": "/landing/flip-a-coin-w1/static/media/promo-star.24cc8691.svg"
+  },
+  {
+    "revision": "7278b07fd004efc25392062f926af331",
+    "url": "/landing/flip-a-coin-w1/static/media/promo-footer.7278b07f.svg"
+  },
+  {
+    "revision": "9714cf48e4048f2da3a34fbce882bb93",
+    "url": "/landing/flip-a-coin-w1/static/media/arrow-right.9714cf48.svg"
+  },
+  {
+    "revision": "04086754c61811c02797d24a94c0883d",
+    "url": "/landing/flip-a-coin-w1/static/media/close.04086754.svg"
+  },
+  {
+    "revision": "70bbe91b552997eca3c3c61829c4bc67",
+    "url": "/landing/flip-a-coin-w1/index.html"
+  }
+];
